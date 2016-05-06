c4c074fb15deefba610b3eae81b2e78a7447d105
王鑫 <KevinW@promote.cache-dns.local>
2016-05-06 10:16:09 +0800

    初始化项目

---
+++ 百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by 王鑫 on 16/5/5.
+//  Copyright © 2016年 KW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 百思不得姐/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  百思不得姐
+//
+//  Created by 王鑫 on 16/5/5.
+//  Copyright © 2016年 KW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
